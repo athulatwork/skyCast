f67d8a9f5f3cd3f23e2b8e65741a715b14345eab
athulatwork <blueblackathul@gmail.com>
2026-05-28 22:21:12 +0530

    Added backend weather api integration

---
+++ SkyCast/backend/whetherApp/src/test/java/com/whetherApp/WhetherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whetherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhetherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
